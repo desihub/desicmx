ac35e231a6e20cc07dc592b7a5278f3f0e1e0e09
Segev BenZvi <sybenzvi@gmail.com>
2019-11-06 12:51:45 -0800

    Access sframe dither sequence.

--- analysis/dither/ditherdata.py
+++ analysis/dither/ditherdata.py
@@ -4,6 +4,9 @@ from glob import glob
 from astropy.table import Table
 from astropy.io import fits
 
+import numpy as np
+
+
 class DitherSequence:
     """Access class to dithering sequence data from nightwatch or redux
     files."""
@@ -30,7 +33,9 @@ class DitherSequence:
         # Extract the list of exposures on disk.
         self._exposure_files = self._getfilenames()
 
-        # Construct fiber data.
+        # Construct fiber output.
+        self._exposure_table = self._buildtable()
+        self._exposure_table.write('test.fits', overwrite=True)
 
     def _getfilenames(self):
         """Return a list of exposures and filenames given an INI configuration.
@@ -73,6 +78,68 @@ class DitherSequence:
 
         return exfiles
 
+    def _buildtable(self):
+        """Loop through the exposure list and construct an observation
+        table."""
+
+        tabrows = []
+
+        for expid, exfiles in self._exposure_files.items():
+            specflux_b, specflux_r, specflux_z = [], [], []
+            tab = None
+
+            if len(exfiles) == 0:
+                continue
+
+            print(expid)
+            for exfile in exfiles:
+                print(exfile)
+                hdu = fits.open(exfile)
+
+                # Assume sframe format.
+                wave = hdu['WAVELENGTH'].data
+                fluxhead = hdu['FLUX'].header
+                fluxdata = hdu['FLUX'].data
+                ivardata = hdu['IVAR'].data
+                fibermap = hdu['FIBERMAP'].data
+
+                target_id = fibermap['TARGETID']
+                target_ra = fibermap['TARGET_RA']
+                target_dec = fibermap['TARGET_DEC']
+                fiber = fibermap['FIBER']
+                objtype = fibermap['OBJTYPE']
+                flux_r = fibermap['FLUX_R']
+                x, y = [fibermap['FIBERASSIGN_{}'.format(val)] for val in ('X', 'Y')]
+
+                camera = fluxhead['CAMERA'][0].upper()
+
+                for i, fiber_id in enumerate(fiber):
+                    flux = fluxdata[i]
+                    if camera == 'B':
+                        specflux_b.append(np.trapz(flux, wave))
+                    elif camera == 'R':
+                        specflux_r.append(np.trapz(flux, wave))
+                    else:
+                        specflux_z.append(np.trapz(flux, wave))
+
+            for j in range(len(fiber)):
+                tabrows.append((expid,
+                                target_id[j], target_ra[j], target_dec[j],
+                                fiber[j], objtype[j], flux_r[j],
+                                specflux_b[j], specflux_r[j], specflux_z[j],
+                                x[j], y[j]))
+
+            break
+
+        tab = Table(rows=tabrows,
+                    names=('EXPID', 'TARGETID', 'TARGET_RA', 'TARGET_DEC',
+                           'FIBER', 'OBJTYPE', 'FLUX_R',
+                           'SPECFLUX_B', 'SPECFLUX_R', 'SPECFLUX_Z',
+                           'XFOCAL', 'YFOCAL'),
+                    meta={'EXTNAME' : 'DITHER'})
+
+        return tab
+
     def __str__(self):
         output = []
         for ex, files in self._exposure_files.items():
@@ -82,3 +149,5 @@ class DitherSequence:
             output.append(filenames)
 
         return '\n'.join(output)
+
+
