f8de2f95e66d4ce1dac9729da590d9f624740951
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2019-12-15 23:13:26 -0700

    Update generator to Connie's version.

--- bin/dither_seq_json.py
+++ bin/dither_seq_json.py
@@ -30,9 +30,6 @@ parser.add_argument('-r', '--deltara', default=0, type=float,
                     help='Ra offset of center of raster from current position')
 parser.add_argument('-d', '--deltadec', default=0, type=float,
                     help='Dec offset of center of raster from current position')
-parser.add_argument('-s', '--split', dest='split', action='store_true',
-                    default=False,
-                    help='Split JSON scripts for two ICS instances')
 
 args = parser.parse_args()
 
@@ -51,76 +48,42 @@ gfa_script = []     # Independent GFA JSON script.
 spec_script = []    # Independent spectrograph JSON script.
 dith_script = []    # Single interleaved GFA+spectrograph script.
 
-gfa_script.append({'sequence': 'Action',
-                   'action'  : 'slew',
-                   'deltara' : RA.value,
-                   'deltadec': DEC.value
-                  })
-dith_script.append(gfa_script[-1])
-
 for j, (dx, dy) in enumerate(zip(stepx, stepy)):
     ra = ra + dx
     dec = dec + dy
 
-    # GFA sequence:
-    if j > 0:
-        gfa_script.append({'sequence': 'Break'})
-        gfa_script.append({'sequence': 'Action',
-                           'action'  : 'slew',
-                           'deltara' : dx.value,
-                           'deltadec': dy.value
-                          })
-        dith_script.append(gfa_script[-1])
+    # Start a guide sequence.
+    dith_script.append({'sequence'         : 'Guide',
+                        'flavor'           : 'science',
+                        'exptime'          : 5.0,
+                        'action'           : 'start_guiding',
+                        'acquisition_time' : 15.0,
+                        'deltara'          : dx.value,
+                        'deltadec'         : dy.value,
+                        'correct_for_adc'  : False,
+                        'usetemp'          : False,
+                        'uselut'           : False,
+                        'program'          : 'Dither tile_id {:05d} ({:g} {:g})'.format(tile_id, ra.to('arcsec').value, dec.to('arcsec').value)
+                        })
 
-    gfa_script.append({'sequence': 'GFA',
-                       'flavor'  : 'science',
-                       'exptime' : 60.0,
-                       'count'   : 1,
-                       'correct_for_adc' : False,
-                       'program' : "Dither tile_id {:05d} ({:g},{:g})".format( tile_id, 
-                                                                                  (ra).to('arcsec').value,
-                                                                                  (dec).to('arcsec').value
-                                                                                )
-                      })
-    dith_script.append(gfa_script[-1])
-    dith_script.append({'sequence' : 'Break'})
+    # Take a spectrograph exposure.
+    dith_script.append({'sequence'         : 'Spectrographs',
+                        'flavor'           : 'science',
+                        'obstype'          : 'SCIENCE',
+                        'correct_for_adc'  : False,
+                        'usetemp'          : False,
+                        'uselut'           : False,
+                        'exptime'          : 60.0,
+                        'program'          : 'Dither tile_id {:05d} ({:g} {:g})'.format(tile_id, ra.to('arcsec').value, dec.to('arcsec').value)
+                        })
 
-    # Spectrograph sequence:
-    spec_script.append({'sequence' : 'Spectrographs',
-                        'flavor'   : 'science',
-                        'count'    : 1,
-                        'exptime'  : 60.0,
-                        'program'  : "Dither tile_id {:05d} ({:g},{:g})".format( tile_id, 
-                                                                                    (ra).to('arcsec').value,
-                                                                                    (dec).to('arcsec').value),
-                        'obstype'  : "SCIENCE"
-                       })
-    dith_script.append(spec_script[-1])
-    dith_script.append({'sequence' : 'Break'})
+    # Break, then stop guiding at the beginning of the *next* sequence.
+    dith_script.append({'sequence'         : 'Break'})
+    dith_script.append({'sequence'         : 'Guide',
+                        'action'           : 'stop_guiding'
+                        })
 
-    if j+1 < len(stepx):
-        spec_script.append({'sequence' : 'Break'})
-
-gfa_script.append({'sequence': 'Action',
-                   'action'  : 'slew',
-                   'deltara' : -RA.value,
-                   'deltadec': -DEC.value
-                  })
-dith_script.append(gfa_script[-1])
-
-
-if args.split:
-    # Split JSON GFA+spectrograph scripts into two files.
-    gfa_filename = 'gfaseq_tile_id{:05d}_{}arcsec_dra{}_ddec{}.json'.format(tile_id, step.value, args.deltara, args.deltadec)
-    spec_filename = 'specseq_tile_id{:05d}_{}arcsec_dra{}_ddec{}.json'.format(tile_id, step.value, args.deltara, args.deltadec)
-
-    json.dump(gfa_script, open(gfa_filename, 'w'), indent=4)
-    json.dump(spec_script, open(spec_filename, 'w'), indent=4)
-
-    print('Use {} in the DESI observer console.'.format(gfa_filename))
-    print('Use {} in the spectrograph ICS console.'.format(spec_filename))
-else:
-    # Dump JSON GFA+spectrograph script into one file.
-    dith_filename = 'dithseq_tile_id{:05d}_{}arcsec_dra{}_ddec{}.json'.format(tile_id, step.value, args.deltara, args.deltadec)
-    json.dump(dith_script, open(dith_filename, 'w'), indent=4)
-    print('Use {} in the DESI observer console.'.format(dith_filename))
+# Dump JSON GFA+spectrograph script into one file.
+dith_filename = 'dithseq_tile_id{:05d}_{}arcsec_dra{}_ddec{}.json'.format(tile_id, step.value, args.deltara, args.deltadec)
+json.dump(dith_script, open(dith_filename, 'w'), indent=4)
+print('Use {} in the DESI observer console.'.format(dith_filename))
