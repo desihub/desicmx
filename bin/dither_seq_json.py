f4a5d2b884fa848ac7c8e4d004435608e314498e
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-01-22 22:05:16 -0500

    Enable explicit 3x3 and 5x5 rasters.

--- bin/dither_seq_json.py
+++ bin/dither_seq_json.py
@@ -30,6 +30,8 @@ parser.add_argument('-r', '--tilera', required=True, type=float,
                     help='Central RA of the tile [deg]')
 parser.add_argument('-d', '--tiledec', required=True, type=float,
                     help='Central Dec of the tile [deg]')
+parser.add_argument('-p' '--pattern', dest='pattern', default='3x3',
+                    help='Raster pattern [3x3, 5x5]')
 parser.add_argument('--deltara', default=0, type=float,
                     help='Ra offset of center of raster from current position')
 parser.add_argument('--deltadec', default=0, type=float,
@@ -47,8 +49,26 @@ tile_id = args.tileid
 tile_ra = args.tilera
 tile_dec = args.tiledec
 
-stepx = np.asarray([ 0,  1, -1, -1,  0,  1,  1,  0, -1, -1,  1])*step
-stepy = np.asarray([ 0,  1,  0,  0, -1,  0,  0, -1,  0,  0,  1])*step
+
+# Standard raster: 3x3 with 3 visits to (0,0), 11 exposures total.
+if args.pattern == '3x3':
+    stepx = np.asarray([ 0,  1, -1, -1,  0,  1,  1,  0, -1, -1,  1])*step
+    stepy = np.asarray([ 0,  1,  0,  0, -1,  0,  0, -1,  0,  0,  1])*step
+# Extended raster: 5x5 with 3 visits to (0,0), 27 exposures in total.
+elif args.pattern == '5x5':
+    # Inner 3x3:
+    stepx = np.asarray([ 0,  1, -1, -1,  0,  0,  1,  1,  0, -1])*step
+    stepy = np.asarray([ 0,  1,  0,  0, -1, -1,  0,  0,  1,  0])*step
+
+    # Outer 5x5:
+    _stepx = np.asarray([ 2, -1, -1, -1, -1,  0,  0,  0,  0,  1,  1,  1,  1,  0,  0,  0, -2])*step
+    _stepy = np.asarray([ 2,  0,  0,  0,  0, -1, -1, -1, -1,  0,  0,  0,  0,  1,  1,  1, -1])*step
+
+    stepx = np.concatenate((stepx, _stepx))
+    stepy = np.concatenate((stepy, _stepy))
+else:
+    raise SystemExit('Invalid raster pattern {}'.format(args.pattern))
+
 
 dith_script = []    # Single interleaved guider+spectrograph script.
 
@@ -94,6 +114,7 @@ for j, (dx, dy) in enumerate(zip(stepx, stepy)):
 #                        })
 
 # Dump JSON guider + spectrograph script into one file.
-dith_filename = 'dithseq_tile_id{:05d}_{}arcsec_dra{}_ddec{}.json'.format(tile_id, step.value, args.deltara, args.deltadec)
+dith_filename = 'dithseq_tile_id{:05d}_{}arcsec_dra{}_ddec{}_{}.json'.format(tile_id, step.value, args.deltara, args.deltadec, args.pattern)
+
 json.dump(dith_script, open(dith_filename, 'w'), indent=4)
 print('Use {} in the DESI observer console.'.format(dith_filename))
