6120719583222f9579bd22995a3f4fdc638de10a
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-02-05 06:08:16 -0500

    Document new modes.

--- bin/dither_seq_json.py
+++ bin/dither_seq_json.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+"""Generate guider and spectrograph ICS sequences for running telescope dither
+tests. Can run in two modes:
+
+1. rastermode, which will move the telescope boresight given a tile ID and step size. One can specify an offset of the raster center from the current location; the script will return to the current location at the end. And the labels on the raster positions will be relative to the current location. This is so that one can do multiple rasters in the same coordinate system.
+
+2. fibermode, which will move the positioners given a range of consecutive tile IDs.  
+
+Based on instructions available at
+
+https://desi.lbl.gov/trac/wiki/DESIOperations/DithSeq
+"""
 
 import os
 import json
